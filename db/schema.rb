1f1c8e54b39494f658e702f0a3f599983d26c320
Gaurav <gorakshnath.sanas@acldigital.com>
2022-05-25 17:23:31 +0530

    user registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_093653) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_110921) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_093653) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"

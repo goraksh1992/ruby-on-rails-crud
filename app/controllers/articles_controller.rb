92a51a77b079b938eaa65d5ed225122f817337b4
Gaurav <gorakshnath.sanas@acldigital.com>
2022-05-25 10:46:14 +0530

    changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
 
+    before_action :get_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -13,39 +14,45 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
         @article = Article.new(
-            params.require(:article).permit(:title, :description)
+            article_params
         )
 
         if @article.save
             flash[:notice] = "New article added!"
             redirect_to articles_path
         else
-            redirect_to 'new'
+            render 'new'
         end
     end
 
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "New article updated!"
             redirect_to articles_path
         else
-            redirect_to 'new'
+            render 'new'
         end
     end
 
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
-
         redirect_to articles_path
+    end
+
+    private
+
+    def get_article
+        @article = Article.find(params[:id])
+    end 
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 
 end
